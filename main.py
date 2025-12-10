18f2fa2659df9ef3af495b42ea32e85223d33efe
Jonathan Parsons <jonathanaaronparsons@gmail.com>
2025-12-09 20:00:01 -0500

    planning

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+# 
+# ideas:
+# - CLI interface doesn't need to be fancy
+# - Prompt user for various data points
+#   - cash close out, credit card close out, product costs; you get the idea
+# - Once all the data has been confirmed generate a CSV file or excel spreadsheet
+# 
+
+# 
+# REFER TO example.txt
+# 
+# data structure planning:
+#
+# data = [
+# ("cash close out", 1234),
+# ("credit card close out", 4321),
+# ("total product costs", 1234),
+# ("returned meat", 12),
+# ("profit shared", 200)
+# ]
+# 
+
+
 def random_func():
     print("Hello World")
+
+
